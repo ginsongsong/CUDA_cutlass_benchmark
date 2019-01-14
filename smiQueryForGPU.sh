e9d73fe731e90a16864eee1b0eee722075ba8feb
CH <ginsongsong@honghutech.com>
2019-01-14 11:24:35 +0800

    Update smiQueryForGPU.sh
    add watch cmd for monitor status precisely.

--- smiQueryForGPU.sh
+++ smiQueryForGPU.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 nvidia-smi --query-gpu=index,timestamp,pstate,power.draw,clocks.gr,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.used --format=csv -l 1 
+
+#watch -n 1 nvidia-smi --query-gpu=index,timestamp,pstate,power.draw,clocks.gr,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.used --format=csv 
